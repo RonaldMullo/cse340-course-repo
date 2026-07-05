bf4b6db88eb1ff622c480ae3ed3e3d36ecf03c17
RonaldMullo <electroascensores64@gmail.com>
2026-07-05 12:33:04 -0500

    The pages,partials, and static files were added

---
+++ server.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+dotenv.config();
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+const PORT = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('home', { title: 'Home' });
+});
+
+app.get('/organizations', (req, res) => {
+    res.render('organizations', { title: 'Organizations' });
+});
+
+app.get('/projects', (req, res) => {
+    res.render('projects', { title: 'Projects' });
+});
+
+app.get('/categories', (req, res) => {
+    res.render('categories', { title: 'Categories' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://127.0.0.1:${PORT}`);
+  console.log(`Environment: ${NODE_ENV}`);
+});
